9536b84a63339c61f4deb764524253c2f940f2cd
Pranay Kumar Rudra <Pranay Kumar Rudra>
2016-08-17 02:41:37 -0700

    Added hero page and sidebar

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#menu-close').click(function (e) {
+        e.preventDefault();
+        $('#sidebar-wrapper').toggleClass("active");
+    });
+
+    $('#menu-toggle').click(function (e) {
+       e.preventDefault();
+        $('#sidebar-wrapper').toggleClass("active");
+    });
+});
