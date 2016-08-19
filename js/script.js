6ffd035a14790099b6b9abf00aada92850af4092
Pranay Kumar Rudra <Pranay Kumar Rudra>
2016-08-18 19:58:51 -0700

    Added skills section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
-    $('#menu-close').click(function (e) {
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
         e.preventDefault();
-        $('#sidebar-wrapper').toggleClass("active");
+        $("#sidebar-wrapper").toggleClass("active");
     });
-
-    $('#menu-toggle').click(function (e) {
-       e.preventDefault();
-        $('#sidebar-wrapper').toggleClass("active");
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
     });
     
+    //Skills bar
+    $('.skillbar').each(function () {
+       $(this).find('.skillbar-bar').animate({
+          width: $(this).attr('data-percent')
+       }, 5000);
+    });
 });
